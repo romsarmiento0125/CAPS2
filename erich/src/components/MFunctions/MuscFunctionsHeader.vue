999b11b989846dd2837b64cb667e743cf4b53656
romsarmiento0125 <68327800+romsarmiento0125@users.noreply.github.com>
2022-01-03 23:08:39 +0800

    push

---
+++ erich/src/components/MFunctions/MuscFunctionsHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-app-bar
+      color="#FFFFFF"
+      height="auto"
+    >
+      <v-container fluid>
+        <v-row>
+          <v-col
+            cols="5"
+          >
+            <div
+              class="d-flex"
+            >
+              <router-link
+                to="/"
+              >
+                <v-img
+                  contain
+                  src="../../assets/ERICH.svg"
+                  max-height="120px"
+                  max-width="120px"
+                  class="mr-1"
+                ></v-img>
+              </router-link>
+              <v-divider
+                vertical
+              ></v-divider>
+              <p
+                class="text-h5 ml-3 my-0 pt-1 grey--text text--darken-1"
+              >Erich Grocery Store</p>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    data: () => ({
+
+    }),
+
+    methods: {
+    },
+
+    beforeMount(){
+    },
+  }
+</script>
